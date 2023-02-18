650a8faeb15fb5891a32142f01ce5df4272025bc
Julia <67640854+julesss-coder@users.noreply.github.com>
2023-02-18 16:25:45 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# prolog-adventures
-Adventure game, family tree, and customer data base in Prolog.
+Three Prolog projects, based on the book "Adventure in Prolog" by Dennis Merritt.
+
+1. A text-based adventure game called "Nani search".
+2. A family tree.
+	I chose to use the family trees of the characters in Jane Austen's novel "Pride and Prejudice". Find their genealogy here: http://larasormiston.blogspot.com/2014/07/a-pride-and-prejudice-relationship-chart.html
+3. A data base of customer orders.
---
+++ customerorders.pl
@@ -0,0 +1,25 @@
+/* Customers */
+customer(hans, linz, aaa).
+customer(maria, wien, aab).
+customer(hermann, obertraun, ccc).
+
+/* Items for sale */
+/* Item has inventory number, name, reorder when stock <= this point. */
+item(001, toast, 200).
+item(002, beer, 300).
+item(003, ham, 200).
+item(004, cheese, 250).
+item(005, ketchup, 30).
+
+/* Inventory */
+/* Stock has item id and amount in stock. */
+stock(001, 201).
+stock(002, 400).
+stock(003, 200).
+stock(004, 200).
+stock(005, 50).
+
+
+
+
+
